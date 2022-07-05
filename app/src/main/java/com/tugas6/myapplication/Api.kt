bf6b3caf7588ab1cb18179dd32672234b5b4bc42
arnoldsyahputra <1910031802034@sar.ac.id>
2022-07-05 22:43:33 +0700

    Initial commit

---
+++ app/src/main/java/com/tugas6/myapplication/Api.kt
@@ -0,0 +1,10 @@
+package com.tugas6.myapplication
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+interface Api {
+    @GET("posts")
+    fun getPosts(): Call<ArrayList<PostResponse>>
+}
---
+++ app/src/main/java/com/tugas6/myapplication/PostResponse.kt
@@ -0,0 +1,7 @@
+package com.tugas6.myapplication
+
+data class PostResponse(
+    val id: Int,
+    val title: String,
+    val body: String
+)
